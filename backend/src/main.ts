e9765749f898584b185d077b634046a48a63c753
giabao1340 <luonggiabao060904@gmail.com>
2026-07-10 20:46:41 +0700

    change dbUrl

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,8 +9,14 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors();
-
+  // main.ts
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://kai-store-psi.vercel.app', // ← thêm domain Vercel
+    ],
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT || 3001);
   console.log('🚀 Server running on http://localhost:3001');
